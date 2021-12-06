01729014704b4fe37660d4fd76c84956fbb37828
Francisco Oropeza <franoropeza@outlook.com>
2021-12-05 21:24:39 -0300

    menu creado

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+
+        <navbar>
+            <nav>
+                <div class="nav-wrapper">
+                <a href="index.html" class="brand-logo">Tinaz</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sandalias.html">Sandalias</a></li>
+                    <li><a href="botas.html">Botas</a></li>
+                    <li><a href="zuecos.html">Zuecos</a></li>
+                </ul>
+                </div>
+            </nav>
+        </navbar>
+
+    )
+}
+
+export default Navbar
